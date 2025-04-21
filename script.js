64df7ba2f4cb0d07b3e0a209e1a040f19c6e4c39
Darshan D <darshandmysuru@gmail.com>
2025-04-21 13:00:27 +0530

    The version 0

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('Hello');
+console.log('\n I love Chocolate');
+
+//window.alert('You are learning Javascript');
+//window.alert('oh ok');
+document.getElementById("myH").textContent = 'Welcome !!!';
+document.getElementById("myPar").textContent = 'I like Chocolate';
+
+let yax ;
+yax = 209;
+window.alert('The value of yax is\t'+yax);
+
+/*
+this is a comment
+*/
+
+//You know this is single line comment
