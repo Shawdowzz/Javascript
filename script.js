bc687147cbe921ec1c26dc157a18eb7ec4d9f459
Darshan D <darshandmysuru@gmail.com>
2025-04-22 11:55:20 +0530

    version 2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById("myPar").textContent = 'I like Chocolate';
 let yax ;
 yax = 209;
 console.log(typeof yax);
-window.alert(`The value of yax is ${yax}`);
+//window.alert(`The value of yax is ${yax}`);
 let names = "Darshan ";
 console.log(`your name is ${names}`);
 console.log(`Line 3
@@ -23,3 +23,14 @@ this is a comment
 */
 
 //You know this is single line comment
+
+let LiftUp = true;
+console.log(`Is the Lift Up? : ${LiftUp}`);
+
+
+document.getElementById("p1").textContent = `Is the lift in the upmost floor ? : ${LiftUp}`;
+
+let say = 34;
+console.log(say+1);
+console.log(say**2); // power of 2^2
+console.log(say%2);
