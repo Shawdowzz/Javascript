03ae8189d3c9ff4840ae664b5b6050fde4b85c79
Darshan D <darshandmysuru@gmail.com>
2025-05-14 17:24:58 +0530

    version 11.2

--- Project_2/script.js
+++ Project_2/script.js
@@ -1,3 +1,17 @@
 const increasebtn =document.getElementById('incr');
 const decreasebtn = document.getElementById('decr');
 const resetbtn = document.getElementById('reset');
+const count = document.getElementById('count');
+let cnt = 0;
+increasebtn.onclick = function(){
+    cnt ++;
+    count.textContent = cnt;
+}
+resetbtn.onclick = function(){
+    cnt = 0;
+    count.textContent = cnt;
+}
+decreasebtn.onclick = function(){
+    cnt --;
+    count.textContent = cnt;
+}
