249685c0d1c353285c52d30016903c1bd824f012
Darshan D <darshandmysuru@gmail.com>
2025-05-15 12:14:23 +0530

    version 11.3 Js learning

--- Project_2/script.js
+++ Project_2/script.js
@@ -11,7 +11,33 @@ resetbtn.onclick = function(){
     cnt = 0;
     count.textContent = cnt;
 }
+
 decreasebtn.onclick = function(){
     cnt --;
     count.textContent = cnt;
 }
+
+console.log(Math.PI);
+console.log(Math.E);
+let x =3.51;
+x = Math.round(x);
+console.log(x);
+x = 3.51;
+x = Math.floor(x);
+console.log(x);
+ x =Math.ceil(x);
+ console.log(x);
+
+ let n = -20;
+ let m = 2;
+ let v;
+ //v = Math.sqrt(n);
+ //v = Math.log(n);
+ //v =Math.cos(n); //in radians
+ //v =Math.tan(n);
+ //v = Math.sin(n);
+//  v =Math.abs(n); // absolute value (opposite value)
+
+
+
+ console.log(v);
