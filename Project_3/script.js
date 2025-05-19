632aaff338f40c7cd84e9c1c9c7d756bd5923311
Darshan D <darshandmysuru@gmail.com>
2025-05-19 17:14:41 +0530

    version 11.5 project 3

--- Project_3/script.js
+++ Project_3/script.js
@@ -1,8 +1,17 @@
-let randomN = document.getElementById('btn');
-let max = 6;
-let min = 1;
-
-randomN.onclick = function(){
-   randomN= Math.floor(Math.random()*6)+min;
-   randomN= document.getElementById('gen')
+let gen = document.getElementById("gen");
+let btn = document.getElementById('btn');
+btn.onclick= function(){
+    let min = parseInt(document.getElementById('Min').value);
+    let max = parseInt(document.getElementById('Max').value);
+    
+    if(isNaN(min)|| isNaN(max)){
+        gen.textContent = "Please Enter valid number";
+        return;
+    }
+    if(min>max){
+        gen.textContent = "Minimum is greater than Maximum";
+        return;
+    }
+    let rando = Math.floor(Math.random()*(max-min+1))+min;
+    gen.textContent= `The Random Number is ${rando}`;
 }
