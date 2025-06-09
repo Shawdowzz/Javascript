74fca13be7ebb6f1542cf0d28ad1b24865a709bb
Darshan D <darshandmysuru@gmail.com>
2025-06-09 23:00:24 +0530

    to-do-list v1.1

--- React/Project/to_do_list_v1/src/App.jsx
+++ React/Project/to_do_list_v1/src/App.jsx
@@ -5,8 +5,28 @@ function App() {
     <h1>To-Do List</h1>
       <div class="container text-center">
   <div class="row">
-    <div class="col-8">col-8</div>
-    <div class="col-4">col-4</div>
+    <div class="col-4">
+      <input type="text" placeholder="To Do Task"/>
+    </div>
+    <div class="col-4">
+      <input type="date"/>
+    </div>
+    <div class="col-2">
+      <button type="button" class="btn btn-outline-primary">Add</button>
+    </div>
+  </div>
+
+  <div class="container text-center">
+   <div class="row">
+    <div class="col-4">
+      Buy Chocolate
+    </div>
+    <div class="col-4">
+      2/4/2025</div>
+    </div>
+    <div class="col-2">
+      <button type="button" class="btn btn-secondary">Secondary</button>
+    </div>
   </div>
 </div>
   </center>
