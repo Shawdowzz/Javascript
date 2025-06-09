e905ae8e1249726a70fc921f3fd5135d9b43a6a1
Darshan D <darshandmysuru@gmail.com>
2025-06-09 17:21:55 +0530

    React learning

---
+++ React/create-first-comp/src/App.jsx
@@ -0,0 +1,11 @@
+import {Button} from "./Button.jsx"
+import {Kano} from "./Button.jsx"
+
+function App(){
+  return  <div>
+    <h1>Hello</h1>
+    <Button></Button>
+    <Kano></Kano>
+  </div>
+}
+export default App;
---
+++ React/create-first-comp/src/Button.jsx
@@ -0,0 +1,6 @@
+export function Button(){
+  return <button>Hello</button>
+}
+export function Kano(){
+  return <h3>hi</h3>
+}
