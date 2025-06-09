5741c81b6ee97183097205b4b30da9bbbb863e55
Darshan D <darshandmysuru@gmail.com>
2025-06-09 22:48:15 +0530

    To-do-List V1

---
+++ React/Project/to_do_list_v1/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  
+
+  return <center class='to-do-container'>
+    <h1>To-Do List</h1>
+      <div class="container text-center">
+  <div class="row">
+    <div class="col-8">col-8</div>
+    <div class="col-4">col-4</div>
+  </div>
+</div>
+  </center>
+}
+
+export default App;
