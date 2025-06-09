0e79f7d263b478412327d47e8e423b33cbd77824
Darshan D <darshandmysuru@gmail.com>
2025-06-09 18:12:32 +0530

    React components styles

--- React/create-first-comp/src/RandomNum.jsx
+++ React/create-first-comp/src/RandomNum.jsx
@@ -1,5 +1,10 @@
-function Random(){
-  let number = Math.ceil(Math.random()*20);
-  return <h1>Your Random Number is : {number}</h1>
+function Random() {
+  let number = Math.ceil(Math.random() * 20);
+  return (
+    <h1 style={{ backgroundColor: 'yellow', padding: '5px', color: 'white' , fontfamily: 'monospace',margin: '50px' }}>
+      Your Random Number is : {number}
+    </h1>
+  );
 }
-export default Random; 
+
+export default Random;
