cc21020f51b818b83e3c3da54c69918a50d8f170
Darshan D <darshandmysuru@gmail.com>
2025-06-11 12:02:28 +0530

    Ternary Operator in Conditional Rendering

--- React/Fragments/Frag/src/Components/CarMap.jsx
+++ React/Fragments/Frag/src/Components/CarMap.jsx
@@ -1,13 +1,12 @@
 let Cars= () => {
 
-  //let Car = [];
-  let Car = ['BMW','Audi','Toyota','Nissan'];
-  if(Car.length === 0){
-      return <h2>Cars not present</h2>
-    }
+  let Car = [];
+  //let Car = ['BMW','Audi','Toyota','Nissan'];
+  let emty = Car.length === 0 ? <h1>NO cars </h1> : null ;
   return (
     <>
     <h1>Car list -2 </h1>
+    {emty}
     <ul className="list-group">
     {Car.map((items)=>
       (<li key={items} className="list-group-item">{items}</li>
