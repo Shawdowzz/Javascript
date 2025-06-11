d3a52c843b0a0e00f7178741b993131a7aab5d59
Darshan D <darshandmysuru@gmail.com>
2025-06-11 11:53:12 +0530

    Conditional If Else

--- React/Fragments/Frag/src/Components/CarMap.jsx
+++ React/Fragments/Frag/src/Components/CarMap.jsx
@@ -1,12 +1,13 @@
 let Cars= () => {
 
-  let Car = [];
-  //let Car = ['BMW','Audi','Toyota','Nissan'];
+  //let Car = [];
+  let Car = ['BMW','Audi','Toyota','Nissan'];
+  if(Car.length === 0){
+      return <h2>Cars not present</h2>
+    }
   return (
     <>
     <h1>Car list -2 </h1>
-    {Car.length === 0 ? <h1>Cars not present</h1> : null }
-    
     <ul className="list-group">
     {Car.map((items)=>
       (<li key={items} className="list-group-item">{items}</li>
