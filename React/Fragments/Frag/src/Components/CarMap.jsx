db9e6b92ab30ab4217445cf0c6414ca6269b2aa6
Darshan D <darshandmysuru@gmail.com>
2025-06-11 12:06:16 +0530

    Logical operator in CR

--- React/Fragments/Frag/src/Components/CarMap.jsx
+++ React/Fragments/Frag/src/Components/CarMap.jsx
@@ -2,11 +2,11 @@ let Cars= () => {
 
   let Car = [];
   //let Car = ['BMW','Audi','Toyota','Nissan'];
-  let emty = Car.length === 0 ? <h1>NO cars </h1> : null ;
+  //let emty = Car.length === 0 ? <h1>NO cars </h1> : null ;
   return (
     <>
     <h1>Car list -2 </h1>
-    {emty}
+    {Car.length === 0 && <h2>No Cars Avaliable</h2>}
     <ul className="list-group">
     {Car.map((items)=>
       (<li key={items} className="list-group-item">{items}</li>
